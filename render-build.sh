f40eae002790409a11be1cea1e77b623ee6443c9
tungdibui2609 <tungdibui2609@gmail.com>
2025-10-24 19:35:17 +0700

    càiont

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 echo "🧩 Cài Chrome cho Puppeteer..."
 npx puppeteer browsers install chrome
-
-echo "🧩 Cài font Roboto..."
-apt-get update -y && apt-get install -y fonts-roboto fonts-noto fonts-noto-cjk
-
 echo "🚀 Khởi động server..."
 node index.js
